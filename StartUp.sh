9841b487fc109bfbf44cd82ade644f6929242a03
Daniel Pink <dr.pink@gmail.com>
2024-02-20 16:06:41 +1000

    Adding some prompts

--- StartUp.sh
+++ StartUp.sh
@@ -6,23 +6,27 @@ echo "Guided procedure for setting up a RPi"
 echo $line
 
 echo "Updating the distribution and installed programs"
-read "Press key to continue"
+read -n 1 -r -s -p $'Press enter to continue...\n'
 sudo apt update
 sudo apt full-upgrade
 
 echo "Install Minicom"
+read -n 1 -r -s -p $'Press enter to continue...\n'
 sudo apt install minicom
 
+echo "Minicom setup will start. Update the following settings"
 echo "Set the default serial port to /dev/ttyUSB0"
 echo "115200 8N1"
 echo "Hardware flow control OFF"
 echo "Software flow control ON"
 echo "Save as dfl (for default)"
+read -n 1 -r -s -p $'Press enter to continue...\n'
 sudo minicom -s
 
+# How do I install the msp430 BSL tools (python 2 and standalone)
 
-
-echo "Install Tailscale"
+echo "Install and setup Tailscale"
+read -n 1 -r -s -p $'Press enter to continue...\n'
 curl -fsSL https://tailscale.com/install.sh | sh
 
 sudo tailscale up
